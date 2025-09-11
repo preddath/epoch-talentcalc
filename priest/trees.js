2ce08652f0fa3de0ca4dc7460658d7c103446f1b
preddath <30547829+preddath@users.noreply.github.com>
2025-09-11 12:09:43 +0200

    Create trees.js

---
+++ priest/trees.js
@@ -0,0 +1,20 @@
+var trees = {
+  "holy": {
+    "image": "",
+    "total": 0,
+    "max": 60,
+    "talents": []
+  },
+  "discipline": {
+    "image": "",
+    "total": 0,
+    "max": 60,
+    "talents": []
+  },
+  "shadow": {
+    "image": "",
+    "total": 0,
+    "max": 60,
+    "talents": []
+  },
+};
