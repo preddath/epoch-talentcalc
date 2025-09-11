bf504a9d78477b403b3315d5b3b719167d6beb7b
preddath <30547829+preddath@users.noreply.github.com>
2025-09-11 12:10:48 +0200

    Create trees.js

---
+++ rogue/trees.js
@@ -0,0 +1,20 @@
+var trees = {
+  "assassination": {
+    "image": "",
+    "total": 0,
+    "max": 60,
+    "talents": []
+  },
+  "combat": {
+    "image": "",
+    "total": 0,
+    "max": 60,
+    "talents": []
+  },
+  "subtlety": {
+    "image": "",
+    "total": 0,
+    "max": 60,
+    "talents": []
+  },
+};
