a3cc2fb8148e72cd2582fdaa6854fc95a5e44c09
preddath <30547829+preddath@users.noreply.github.com>
2025-09-11 12:11:14 +0200

    Create trees.js

---
+++ shaman/trees.js
@@ -0,0 +1,20 @@
+var trees = {
+  "elemental": {
+    "image": "",
+    "total": 0,
+    "max": 60,
+    "talents": []
+  },
+  "enhancement": {
+    "image": "",
+    "total": 0,
+    "max": 60,
+    "talents": []
+  },
+  "restoration": {
+    "image": "",
+    "total": 0,
+    "max": 60,
+    "talents": []
+  },
+};
