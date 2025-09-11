e4ef9cfd3631eb35389d84052961bfedb1f49b13
preddath <30547829+preddath@users.noreply.github.com>
2025-09-11 12:08:00 +0200

    Create trees.js

---
+++ druid/trees.js
@@ -0,0 +1,20 @@
+var trees = {
+  "balance": {
+    "image": "",
+    "total": 0,
+    "max": 60,
+    "talents": []
+  },
+  "feral": {
+    "image": "",
+    "total": 0,
+    "max": 60,
+    "talents": []
+  },
+  "restoration": {
+    "image": "",
+    "total": 0,
+    "max": 60,
+    "talents": []
+  },
+};
