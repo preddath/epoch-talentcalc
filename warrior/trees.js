bbccfa0ed76bb8e92b0a2382cda98341722a79da
preddath <30547829+preddath@users.noreply.github.com>
2025-09-11 12:13:06 +0200

    Create trees.js

---
+++ warrior/trees.js
@@ -0,0 +1,20 @@
+var trees = {
+  "arms": {
+    "image": "",
+    "total": 0,
+    "max": 60,
+    "talents": []
+  },
+  "fury": {
+    "image": "",
+    "total": 0,
+    "max": 60,
+    "talents": []
+  },
+  "protection": {
+    "image": "",
+    "total": 0,
+    "max": 60,
+    "talents": []
+  },
+};
