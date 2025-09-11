fef6a80a1e96b2e51960baf404593608f0c5bd66
preddath <30547829+preddath@users.noreply.github.com>
2025-09-11 12:12:43 +0200

    Create trees.js

---
+++ warlock/trees.js
@@ -0,0 +1,20 @@
+var trees = {
+  "affliction": {
+    "image": "",
+    "total": 0,
+    "max": 60,
+    "talents": []
+  },
+  "destruction": {
+    "image": "",
+    "total": 0,
+    "max": 60,
+    "talents": []
+  },
+  "demonology": {
+    "image": "",
+    "total": 0,
+    "max": 60,
+    "talents": []
+  },
+};
