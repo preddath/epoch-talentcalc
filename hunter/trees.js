a3f08c1de2ed2a0fb03ddf955f9d2af6d2f4907b
preddath <30547829+preddath@users.noreply.github.com>
2025-09-11 12:08:48 +0200

    Create trees.js

---
+++ hunter/trees.js
@@ -0,0 +1,20 @@
+var trees = {
+  "marksmanship": {
+    "image": "",
+    "total": 0,
+    "max": 60,
+    "talents": []
+  },
+  "beast-mastery": {
+    "image": "",
+    "total": 0,
+    "max": 60,
+    "talents": []
+  },
+  "survival": {
+    "image": "",
+    "total": 0,
+    "max": 60,
+    "talents": []
+  },
+};
