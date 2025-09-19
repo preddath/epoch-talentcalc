8841f7cf113621e4b27f04be95af0eddf0a89794
Josh <jrheim267@gmail.com>
2025-09-19 20:01:01 +0200

    Structure Trees Shaman

--- shaman/trees.js
+++ shaman/trees.js
@@ -3,18 +3,869 @@ var shaman = {
     "image": "",
     "total": 0,
     "max": 60,
-    "talents": []
+    "talents": [
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 0,
+        "row": 0,
+        "col": 1,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 1,
+        "row": 0,
+        "col": 2,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 2,
+        "row": 1,
+        "col": 0,
+        "max": 2,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 3,
+        "row": 1,
+        "col": 1,
+        "max": 3,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 4,
+        "row": 1,
+        "col": 2,
+        "max": 3,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 5,
+        "row": 2,
+        "col": 0,
+        "max": 1,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 6,
+        "row": 2,
+        "col": 1,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 7,
+        "row": 2,
+        "col": 2,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 8,
+        "row": 3,
+        "col": 0,
+        "max": 2,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 9,
+        "row": 3,
+        "col": 1,
+        "max": 3,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 10,
+        "row": 3,
+        "col": 3,
+        "max": 3,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 11,
+        "row": 4,
+        "col": 0,
+        "max": 2,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 12,
+        "row": 4,
+        "col": 1,
+        "max": 1,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 14,
+        "row": 4,
+        "col": 3,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 15,
+        "row": 5,
+        "col": 0,
+        "max": 3,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 16,
+        "row": 5,
+        "col": 2,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 17,
+        "row": 6,
+        "col": 1,
+        "max": 1,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png",
+        "type": "ability"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 18,
+        "row": 6,
+        "col": 2,
+        "max": 3,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 19,
+        "row": 7,
+        "col": 1,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 20,
+        "row": 8,
+        "col": 1,
+        "max": 1,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png",
+        "type": "ability"
+      },
+    ]
   },
   "enhancement": {
     "image": "",
     "total": 0,
     "max": 60,
-    "talents": []
+    "talents": [
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 0,
+        "row": 0,
+        "col": 1,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 1,
+        "row": 0,
+        "col": 2,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 2,
+        "row": 1,
+        "col": 0,
+        "max": 2,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 3,
+        "row": 1,
+        "col": 1,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 4,
+        "row": 1,
+        "col": 2,
+        "max": 2,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 8,
+        "row": 1,
+        "col": 3,
+        "max": 3,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 5,
+        "row": 2,
+        "col": 0,
+        "max": 2,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 6,
+        "row": 2,
+        "col": 2,
+        "max": 1,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 7,
+        "row": 2,
+        "col": 3,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 9,
+        "row": 3,
+        "col": 1,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 10,
+        "row": 3,
+        "col": 2,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 11,
+        "row": 4,
+        "col": 0,
+        "max": 2,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 12,
+        "row": 4,
+        "col": 1,
+        "max": 1,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 14,
+        "row": 4,
+        "col": 2,
+        "max": 3,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 15,
+        "row": 5,
+        "col": 0,
+        "max": 3,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 16,
+        "row": 5,
+        "col": 3,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 17,
+        "row": 6,
+        "col": 0,
+        "max": 3,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 13,
+        "row": 6,
+        "col": 1,
+        "max": 1,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 18,
+        "row": 6,
+        "col": 2,
+        "max": 1,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png",
+        "type": "ability"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 19,
+        "row": 7,
+        "col": 1,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 20,
+        "row": 8,
+        "col": 1,
+        "max": 1,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png",
+        "type": "ability"
+      },
+    ]
   },
   "restoration": {
     "image": "",
     "total": 0,
     "max": 60,
-    "talents": []
+    "talents": [
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 0,
+        "row": 0,
+        "col": 1,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 1,
+        "row": 0,
+        "col": 2,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 2,
+        "row": 1,
+        "col": 0,
+        "max": 2,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 3,
+        "row": 1,
+        "col": 1,
+        "max": 3,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 4,
+        "row": 1,
+        "col": 2,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 5,
+        "row": 2,
+        "col": 0,
+        "max": 3,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 6,
+        "row": 2,
+        "col": 1,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 7,
+        "row": 2,
+        "col": 2,
+        "max": 1,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 10,
+        "row": 2,
+        "col": 3,
+        "max": 3,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 8,
+        "row": 3,
+        "col": 1,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 9,
+        "row": 3,
+        "col": 2,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 11,
+        "row": 4,
+        "col": 0,
+        "max": 3,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 12,
+        "row": 4,
+        "col": 2,
+        "max": 1,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png",
+        "type": "ability"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 14,
+        "row": 4,
+        "col": 3,
+        "max": 3,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 16,
+        "row": 5,
+        "col": 2,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 17,
+        "row": 6,
+        "col": 1,
+        "max": 1,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png",
+        "type": "ability"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 18,
+        "row": 6,
+        "col": 2,
+        "max": 5,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 19,
+        "row": 7,
+        "col": 1,
+        "max": 3,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 15,
+        "row": 7,
+        "col": 2,
+        "max": 2,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png"
+      },
+
+
+      {
+        "name": "Improved ",
+        "description": "Increases  by %p%.",
+        "values": {
+          "%p": 3
+        },
+        "id": 20,
+        "row": 8,
+        "col": 1,
+        "max": 1,
+        "current": 0,
+        "image": "ability_rogue_eviscerate.png",
+        "type": "ability"
+      },
+    ]
   },
 };
